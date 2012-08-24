114c1ea6ed79d88d6124227c60fede9b58366899
arunprasanna <arun.prasanna@photoninfotech.net>
2012-08-24 15:26:51 +0530

    [artf521831] new fields added for file upload

--- src/main/java/com/photon/phresco/model/ArchetypeInfo.java
+++ src/main/java/com/photon/phresco/model/ArchetypeInfo.java
@@ -4,13 +4,23 @@ import com.photon.phresco.commons.model.Element;
 
 public class ArchetypeInfo extends Element{
     
-    private String groupId;
+	private String groupId;
     private String artifactId;
     private String version;
     private String projectGroupId;
+    //for file upload
+    private boolean mavenJar;
+	private boolean success;
     
     public ArchetypeInfo() {
     }
+    
+    public ArchetypeInfo(String groupId, String artifactId, String version) {
+		super();
+		this.groupId = groupId;
+		this.artifactId = artifactId;
+		this.version = version;
+	}
 
     public String getGroupId() {
         return groupId;
@@ -43,8 +53,24 @@ public class ArchetypeInfo extends Element{
     public void setProjectGroupId(String projectGroupId) {
         this.projectGroupId = projectGroupId;
     }
+    
+    public boolean isMavenJar() {
+		return mavenJar;
+	}
 
-    @Override
+	public void setMavenJar(boolean mavenJar) {
+		this.mavenJar = mavenJar;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	@Override
     public String toString() {
         return "ArchetypeInfo [groupId=" + groupId + ", artifactId="
                 + artifactId + ", version=" + version + ", projectGroupId="
