55ecb79a14536f6bd44efe598b573399c040907b
arun-pr <arun.prasanna@photoninfotech.net>
2012-09-18 12:30:18 +0530

    [artf535035] Changes in Features Bean (ModuleGroup and Module changes)

--- src/main/java/com/photon/phresco/model/ModuleGroup.java
+++ src/main/java/com/photon/phresco/model/ModuleGroup.java
@@ -25,7 +25,6 @@ import java.util.List;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
-import com.photon.phresco.model.Documentation.DocumentationType;
 import com.photon.phresco.util.SizeConstants;
 
 @XmlRootElement
@@ -35,38 +34,24 @@ public class ModuleGroup implements Serializable {
 
 	//String id of the module. this will be groupId:artifactId:
 	private String id;
-	private String moduleId;
-	private String groupId;
-	private String artifactId;
 	private String type;
 	private String name;
-	private String description;
-	private String vendor;
 	private boolean core;
-	private boolean required;
-	private List<Documentation> docs;
 	private String techId;
 	private List<Module> versions = new ArrayList<Module>(SizeConstants.SIZE_VERSIONS_MAP);
 	private String imageURL;
 	private boolean system;
 	private String customerId;
-	private String helpText;
 	
 	public ModuleGroup() {
 	}
 
-	public ModuleGroup(String id, String name,String groupId,String artifactId,String type,String vendor,boolean core,boolean required,List<Documentation> docs,List<Module> modules,String helpText) {
+	public ModuleGroup(String id, String name, String groupId, String artifactId, String type, boolean core, List<Module> modules) {
 		this.id = id;
 		this.name = name;
-		this.groupId = groupId;
-		this.artifactId = artifactId;
 		this.type = type;
-		this.vendor = vendor;
 		this.core = core;
-		this.required = required;
-		this.docs = docs;
 		this.versions = modules;
-		this.helpText = helpText;
 	}
 
 	public String getId() {
@@ -75,22 +60,6 @@ public class ModuleGroup implements Serializable {
 
 	public void setId(String id) {
 		this.id = id;
-	}
-
-	public String getGroupId() {
-		return groupId;
-	}
-
-	public void setGroupId(String groupId) {
-		this.groupId = groupId;
-	}
-
-	public String getArtifactId() {
-		return artifactId;
-	}
-
-	public void setArtifactId(String artifactId) {
-		this.artifactId = artifactId;
 	}
 
 	public String getType() {
@@ -109,36 +78,12 @@ public class ModuleGroup implements Serializable {
 		this.name = name;
 	}
 
-	public String getVendor() {
-		return vendor;
-	}
-
-	public void setVendor(String vendor) {
-		this.vendor = vendor;
-	}
-
 	public boolean isCore() {
 		return core;
 	}
 
 	public void setCore(boolean core) {
 		this.core = core;
-	}
-
-	public boolean isRequired() {
-		return required;
-	}
-
-	public void setRequired(boolean required) {
-		this.required = required;
-	}
-
-	public List<Documentation> getDocs() {
-		return docs;
-	}
-
-	public void setDocs(List<Documentation> docs) {
-		this.docs = docs;
 	}
 
 	public List<Module> getVersions() {
@@ -159,35 +104,12 @@ public class ModuleGroup implements Serializable {
 		this.versions = versions;
 	}
 
-	public Documentation getDoc(DocumentationType type) {
-		List<Documentation> docs2 = getDocs();
-		if (docs2 == null || docs2.isEmpty()) {
-			return null;
-		}
-
-		for (Documentation documentation : docs2) {
-			if (type.equals(documentation.getType())) {
-				return documentation;
-			}
-		}
-
-		return null;
-	}
-
 	public String getTechId() {
 		return techId;
 	}
 
 	public void setTechId(String techId) {
 		this.techId = techId;
-	}
-
-	public String getModuleId() {
-		return moduleId;
-	}
-
-	public void setModuleId(String moduleId) {
-		this.moduleId = moduleId;
 	}
 
 	public String getImageURL() {
@@ -213,40 +135,12 @@ public class ModuleGroup implements Serializable {
 	public void setCustomerId(String customerId) {
 		this.customerId = customerId;
 	}
-	
-	public String getDescription() {
-		return description;
-	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
-	public String getHelpText() {
-		return helpText;
-	}
-
-	public void setHelpText(String helpText) {
-		this.helpText = helpText;
-	}
-
-
-	@Override
-	public String toString() {
-		return "ModuleGroup [id=" + id + ", moduleId=" + moduleId
-				+ ", groupId=" + groupId + ", artifactId=" + artifactId
-				+ ", type=" + type + ", name=" + name + ",description=" +description +", vendor=" + vendor
-				+ ", core=" + core + ", required=" + required + ", docs="
-				+ docs + ", techId=" + techId + ", versions=" + versions
-				+ ", imageURL=" + imageURL + ", system=" + system
-				+ ", customerId=" + customerId  
-				+ ", helpText=" + helpText + "]";
-	}
-    
-    
-    /* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	
-
+    @Override
+    public String toString() {
+        return "ModuleGroup [id=" + id + ", type=" + type + ", name=" + name
+                + ", core=" + core + ", techId=" + techId + ", versions="
+                + versions + ", imageURL=" + imageURL + ", system=" + system
+                + ", customerId=" + customerId + "]";
+    }
 }
