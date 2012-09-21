7989bfb95c4776f72f8b63dee29b3993a9eeefda
kumar-s <kumar.selvaraj@photoninfotech.net>
2012-09-21 15:59:59 +0530

    [artf537468]  DataModel Restructuring on client and DAO level

--- src/main/java/com/photon/phresco/param/api/DynamicParameter.java
+++ src/main/java/com/photon/phresco/param/api/DynamicParameter.java
@@ -1,10 +1,10 @@
 package com.photon.phresco.param.api;
 
-import com.photon.phresco.model.ProjectInfo;
+import com.photon.phresco.commons.model.ApplicationInfo;
 import com.photon.phresco.plugins.model.Mojos.Mojo.Configuration.Parameters.Parameter.PossibleValues;
 
 
 public interface DynamicParameter {
 
-	public PossibleValues getValues(ProjectInfo projectInfo);
+	public PossibleValues getValues(ApplicationInfo applicationInfo);
 }
--- src/main/java/com/photon/phresco/param/impl/EnvironmentParameter.java
+++ src/main/java/com/photon/phresco/param/impl/EnvironmentParameter.java
@@ -1,12 +1,12 @@
 package com.photon.phresco.param.impl;
 
-import com.photon.phresco.model.ProjectInfo;
+import com.photon.phresco.commons.model.ApplicationInfo;
 import com.photon.phresco.param.api.DynamicParameter;
 import com.photon.phresco.plugins.model.Mojos.Mojo.Configuration.Parameters.Parameter.PossibleValues;
 
 public class EnvironmentParameter implements DynamicParameter {
 
-	public PossibleValues getValues(ProjectInfo projectInfo) {
+	public PossibleValues getValues(ApplicationInfo applicationInfo) {
 		return null;
 	}
 }
