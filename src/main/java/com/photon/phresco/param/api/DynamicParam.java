22a3b608d60c9e35364041333d02a87350a20d3b
Bharat Kumar Radha <bharatkumar.radha@photoninfotech.net>
2012-09-21 11:06:30 +0530

    [artf537137] - interface and bean for dynamic parameters

---
+++ src/main/java/com/photon/phresco/param/api/DynamicParam.java
@@ -0,0 +1,9 @@
+package com.photon.phresco.param.api;
+
+import java.util.Collection;
+
+
+public interface DynamicParam {
+
+    Collection<PossibleValue> getValues();
+}
---
+++ src/main/java/com/photon/phresco/param/impl/EnvironmentParam.java
@@ -0,0 +1,14 @@
+package com.photon.phresco.param.impl;
+
+import java.util.Collection;
+
+import com.photon.phresco.param.api.DynamicParam;
+import com.photon.phresco.param.model.PossibleValue;
+
+public class EnvironmentParam implements DynamicParam {
+
+    public Collection<PossibleValue> getValues() {
+        return null;
+    }
+
+}
