f3799a49867c64dd6727400bd64546079de5df58
rajeshkumar-ra <rajeshkumar.ravichandra@photoninfotech.net>
2012-10-23 16:36:47 +0530

    [artf551085] EnvironmentParameter class moved

--- src/main/java/com/photon/phresco/param/api/DynamicParameter.java
+++ src/main/java/com/photon/phresco/param/api/DynamicParameter.java
@@ -1,17 +1,21 @@
 package com.photon.phresco.param.api;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
-import com.photon.phresco.commons.model.ApplicationInfo;
 import com.photon.phresco.exception.ConfigurationException;
+import com.photon.phresco.exception.PhrescoException;
 import com.photon.phresco.plugins.model.Mojos;
 
 
 public interface DynamicParameter {
+	
+	String KEY_APP_INFO = "applicationInfo";
+	String KEY_BUILD_NO = "buildNumber";
 
-    public Mojos.Mojo.Configuration.Parameters.Parameter.PossibleValues getValues(ApplicationInfo projectInfo) throws IOException, ParserConfigurationException, SAXException, ConfigurationException;
+    public Mojos.Mojo.Configuration.Parameters.Parameter.PossibleValues getValues(Map<String, Object> map) throws IOException, ParserConfigurationException, SAXException, ConfigurationException, PhrescoException;
 }
