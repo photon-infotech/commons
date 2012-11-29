f5298237f0b38641635ac5f3ca8b5d018b1dd84d
vivek-pa <vivek.parthiban@photoninfotech.net>
2012-11-29 15:57:45 +0530

    Changed name of WinMetroApplicationProcessor.java to WindowsApplicationProcessor.java and added application processors for missing technologies

---
+++ src/main/java/com/photon/phresco/impl/BlackberryApplicationProcessor.java
@@ -0,0 +1,57 @@
+package com.photon.phresco.impl;
+
+import java.util.List;
+
+import com.photon.phresco.api.ApplicationProcessor;
+import com.photon.phresco.commons.model.ApplicationInfo;
+import com.photon.phresco.commons.model.ArtifactGroup;
+import com.photon.phresco.exception.PhrescoException;
+
+public class BlackberryApplicationProcessor implements ApplicationProcessor {
+
+	@Override
+	public void preCreate(ApplicationInfo appInfo) throws PhrescoException {
+		
+		
+	}
+
+	@Override
+	public void preUpdate(ApplicationInfo appInfo) throws PhrescoException {
+		
+		
+	}
+
+	@Override
+	public void postCreate(ApplicationInfo appInfo) throws PhrescoException {
+		
+		
+	}
+
+	@Override
+	public void postUpdate(ApplicationInfo appInfo,
+			List<ArtifactGroup> artifactGroup) throws PhrescoException {
+		
+		
+	}
+
+	@Override
+	public void postConfiguration(ApplicationInfo appInfo)
+			throws PhrescoException {
+		
+		
+	}
+
+	@Override
+	public void preFeatureConfiguration(ApplicationInfo appInfo)
+			throws PhrescoException {
+		
+	}
+
+	@Override
+	public void postFeatureConfiguration(ApplicationInfo appInfo)
+			throws PhrescoException {
+		
+		
+	}
+
+}
---
+++ src/main/java/com/photon/phresco/impl/SiteCoreApplicationProcessor.java
@@ -0,0 +1,58 @@
+package com.photon.phresco.impl;
+
+import java.util.List;
+
+import com.photon.phresco.api.ApplicationProcessor;
+import com.photon.phresco.commons.model.ApplicationInfo;
+import com.photon.phresco.commons.model.ArtifactGroup;
+import com.photon.phresco.exception.PhrescoException;
+
+public class SiteCoreApplicationProcessor implements ApplicationProcessor {
+
+	@Override
+	public void preCreate(ApplicationInfo appInfo) throws PhrescoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void preUpdate(ApplicationInfo appInfo) throws PhrescoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postCreate(ApplicationInfo appInfo) throws PhrescoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postUpdate(ApplicationInfo appInfo,
+			List<ArtifactGroup> artifactGroup) throws PhrescoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postConfiguration(ApplicationInfo appInfo)
+			throws PhrescoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void preFeatureConfiguration(ApplicationInfo appInfo)
+			throws PhrescoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postFeatureConfiguration(ApplicationInfo appInfo)
+			throws PhrescoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
