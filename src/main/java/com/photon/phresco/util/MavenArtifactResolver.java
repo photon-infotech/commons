69a55e4f3f3f1637239529ce6610af5c511a5114
kumar-s <kumar.selvaraj@photoninfotech.net>
2012-12-03 15:28:25 +0530

    [artf572631] Fixed class loading issue in PhrescoDynamicLoader

--- src/main/java/com/photon/phresco/util/MavenArtifactResolver.java
+++ src/main/java/com/photon/phresco/util/MavenArtifactResolver.java
@@ -1,6 +1,12 @@
 package com.photon.phresco.util;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -17,6 +23,8 @@ import org.sonatype.aether.resolution.ArtifactResult;
 import org.sonatype.aether.resolution.DependencyRequest;
 import org.sonatype.aether.util.artifact.JavaScopes;
 import org.sonatype.aether.util.filter.DependencyFilterUtils;
+
+import com.photon.phresco.exception.PhrescoException;
 
 public class MavenArtifactResolver {
 	
@@ -43,7 +51,6 @@ public class MavenArtifactResolver {
 	        S_LOGGER.debug("Local Repository Is" + Utility.getLocalRepoPath());
 	        S_LOGGER.debug("Local Repository Is" + localRepo);
 	    }
-		System.out.println("Local Repository Is  " + localRepo);
 		session.setLocalRepositoryManager(repoSystem.newLocalRepositoryManager(localRepo));
 		
 		String repoName = "phresco"; //TODO: Should be repoInfo.getName()
@@ -51,22 +58,62 @@ public class MavenArtifactResolver {
 		
         DependencyFilter classpathFlter = DependencyFilterUtils.classpathFilter(JavaScopes.COMPILE);
        
-        URL[] urls = new URL[10];
+        List<URL> urls = new ArrayList<URL>();
         for (Artifact artifact : artifacts) {
-	        CollectRequest collectRequest = new CollectRequest();
-	        collectRequest.setRoot( new Dependency( artifact, JavaScopes.COMPILE ) );
-	        collectRequest.addRepository( remoteRepo );
-	
-	        DependencyRequest dependencyRequest = new DependencyRequest(collectRequest, classpathFlter);
-			List<ArtifactResult> artifactResults = repoSystem.resolveDependencies(
-					session, dependencyRequest).getArtifactResults();
-	
-	        int i = 0;
+            CollectRequest collectRequest = new CollectRequest();
+            collectRequest.setRoot( new Dependency( artifact, JavaScopes.COMPILE ) );
+            collectRequest.addRepository( remoteRepo );
+
+            DependencyRequest dependencyRequest = new DependencyRequest( collectRequest, classpathFlter );
+            List<ArtifactResult> artifactResults =
+            	repoSystem.resolveDependencies( session, dependencyRequest ).getArtifactResults();
+        	
 	        for (ArtifactResult artifactResult : artifactResults ) {
-	            urls[i++] =  artifactResult.getArtifact().getFile().toURI().toURL();
+	        	urls.add(artifactResult.getArtifact().getFile().toURI().toURL());
 	        }
         }
-        
-        return urls;
+        URL[] urlArr = new URL[0];
+        return urls.toArray(urlArr);
 	}
+
+	
+	public static URL[] resolveUsingTemp(String repoURL, List<Artifact> artifacts) throws PhrescoException {
+		URL[] urls = new URL[10];
+		for (Artifact artifact : artifacts) {
+			InputStream in = null;
+			FileOutputStream fos = null;
+			try {
+				URL url = new URL(repoURL + artifact.getGroupId().replace(".", "/") + "/" + artifact.getArtifactId() + "/" + artifact.getVersion() + "/" + artifact.getArtifactId() + "-" + artifact.getVersion() + ".jar");
+				URLConnection connection = url.openConnection();
+				in = connection.getInputStream();
+				File filePath = new File(Utility.getPhrescoTemp() + artifact.getArtifactId() + ".jar");
+				filePath.createNewFile();
+				int i = 0;
+				urls[i++] = filePath.toURI().toURL();
+				fos = new FileOutputStream(filePath);
+				byte[] buf = new byte[1024];
+				while (true) {
+					int len;
+					len = in.read(buf);
+					if (len == -1) {
+						break;
+					}
+					fos.write(buf, 0, len);
+				}
+			} catch (Exception e) {
+				throw new PhrescoException(e);
+			} finally {
+				Utility.closeStream(in);
+				if (fos != null) {
+					try {
+						fos.close();
+					} catch (IOException e) {
+						throw new PhrescoException(e);
+					}
+				}
+			}
+		}
+		return urls;
+	}
+	
 }
--- src/main/java/com/photon/phresco/util/PhrescoDynamicLoader.java
+++ src/main/java/com/photon/phresco/util/PhrescoDynamicLoader.java
@@ -1,18 +1,24 @@
 package com.photon.phresco.util;
 
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.sonatype.aether.artifact.Artifact;
 import org.sonatype.aether.util.artifact.DefaultArtifact;
+import org.xml.sax.SAXException;
 
 import com.photon.phresco.api.ApplicationProcessor;
 import com.photon.phresco.api.DynamicParameter;
 import com.photon.phresco.commons.model.ArtifactGroup;
 import com.photon.phresco.commons.model.ArtifactInfo;
 import com.photon.phresco.commons.model.RepoInfo;
+import com.photon.phresco.exception.ConfigurationException;
 import com.photon.phresco.exception.PhrescoException;
 
 public class PhrescoDynamicLoader {
@@ -27,11 +33,11 @@ public class PhrescoDynamicLoader {
 	public DynamicParameter getDynamicParameter(String className) throws PhrescoException {
 		DynamicParameter dynamicParameter;
 		try {
-//			Class<DynamicParameter> apiClass = (Class<DynamicParameter>) Class
-//					.forName(className, true, getURLClassLoader());
-			Class<DynamicParameter> apiClass = (Class<DynamicParameter>) Class.forName(className);
+			Class<DynamicParameter> apiClass = (Class<DynamicParameter>) Class
+					.forName(className, true, getURLClassLoader());
 			dynamicParameter = apiClass.newInstance();
 		} catch (Exception e) {
+			e.printStackTrace();
 			throw new PhrescoException(e);
 		}
 		return dynamicParameter;
@@ -40,11 +46,11 @@ public class PhrescoDynamicLoader {
 	public ApplicationProcessor getApplicationProcessor(String className) throws PhrescoException {
 		ApplicationProcessor applicationProcessor;
 		try {
-//			Class<ApplicationProcessor> apiClass = (Class<ApplicationProcessor>) Class
-//					.forName(className, true, getURLClassLoader());
-			Class<ApplicationProcessor> apiClass = (Class<ApplicationProcessor>) Class.forName(className);
+			Class<ApplicationProcessor> apiClass = (Class<ApplicationProcessor>) Class
+					.forName(className, true, getURLClassLoader());
 			applicationProcessor = apiClass.newInstance();
 		} catch (Exception e) {
+			e.printStackTrace();
 			throw new PhrescoException(e);
 		}
 		return applicationProcessor;
@@ -55,7 +61,7 @@ public class PhrescoDynamicLoader {
 		for (ArtifactGroup plugin : plugins) {
 			List<ArtifactInfo> versions = plugin.getVersions();
 			for (ArtifactInfo artifactInfo : versions) {
-				Artifact artifact = new DefaultArtifact(plugin.getGroupId(), plugin.getArtifactId(), "", artifactInfo.getVersion());
+				Artifact artifact = new DefaultArtifact(plugin.getGroupId(), plugin.getArtifactId(), "jar", artifactInfo.getVersion());
 				artifacts.add(artifact);
 			}
 		}
@@ -65,10 +71,31 @@ public class PhrescoDynamicLoader {
 			artifactURLs = MavenArtifactResolver.resolve(repoInfo.getGroupRepoURL(), repoInfo.getRepoUserName(),
 					repoInfo.getRepoPassword(), artifacts);
 		} catch (Exception e) {
+			e.printStackTrace();
 			throw new PhrescoException(e);
 		}
 		URLClassLoader classLoader = new URLClassLoader(artifactURLs);
 		return classLoader;
 	}
-
+	
+	public static void main(String[] args) throws PhrescoException, IOException, ParserConfigurationException, SAXException, ConfigurationException {
+		RepoInfo repoinfo = new RepoInfo();
+		repoinfo.setRepoName("releases");
+		repoinfo.setRepoUserName("admin");
+//		repoinfo.setRepoPassword("ZGV2cmVwbzI=");
+		repoinfo.setRepoPassword("devrepo2");
+		repoinfo.setGroupRepoURL("http://172.16.17.226:8080/repository/content/groups/public/");
+		ArtifactGroup group = new ArtifactGroup();
+		group.setGroupId("com.photon.phresco.commons");
+		group.setArtifactId("phresco-commons");
+		group.setPackaging("jar");
+		ArtifactInfo info = new ArtifactInfo();
+		info.setVersion("2.0.0.26001");
+		group.setVersions(Arrays.asList(info));
+		PhrescoDynamicLoader loader = new PhrescoDynamicLoader(repoinfo, Arrays.asList(group) );
+		System.out.println("Loader Found..... " + loader);
+		DynamicParameter applicationProcessor = loader.getDynamicParameter("com.photon.phresco.impl.EnvironmentsParameterImpl");
+		applicationProcessor.getValues(null);
+	}
+	
 }
