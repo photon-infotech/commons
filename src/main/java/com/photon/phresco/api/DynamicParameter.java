526826c36ec90ce52a9a59f0f2f58f9edc973e84
kaleeswaran <kaleeswaran.subbaiya@photoninfotech.net>
2013-02-11 14:46:22 +0530

    Deploy to device through CI issue fixed.

--- src/main/java/com/photon/phresco/api/DynamicParameter.java
+++ src/main/java/com/photon/phresco/api/DynamicParameter.java
@@ -25,6 +25,8 @@ public interface DynamicParameter {
 	String KEY_CUSTOMER_ID = "customerId";
 	String KEY_MOJO =  "mojo";
 	String KEY_GOAL = "goal";
+	String KEY_DEVICE_TYPE = "deviceType";
+	String KEY_TRIGGER_SIMULATOR = "triggerSimulator";
 
     public Mojos.Mojo.Configuration.Parameters.Parameter.PossibleValues getValues(Map<String, Object> map) throws IOException, ParserConfigurationException, SAXException, ConfigurationException, PhrescoException;
 }
