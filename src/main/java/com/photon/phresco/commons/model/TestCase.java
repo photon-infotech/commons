5f13852cc9a675dd517ae7737631997c779c6750
jagadeesh-r <jagadeesh.reddy@photoninfotech.net>
2013-03-08 18:32:05 +0530

    [artf643552]: Implementing manual testCases in framework

---
+++ src/main/java/com/photon/phresco/commons/model/TestCase.java
@@ -0,0 +1,123 @@
+package com.photon.phresco.commons.model;
+
+public class TestCase {
+	private String featureId = "";
+	private String requirementId = "";
+	private String testCaseId = "";
+	private String description = "";
+	private String steps = "";
+	private String testCaseype = "";
+	private String testInput = "";
+	private String expectedResult = "";
+	private String actualResult = "";
+	private String status = "";
+	private String priority = "";
+	private String severity = "";
+	private String bugComment = "";
+
+
+	public String getFeatureId() {
+		return featureId;
+	}
+
+	public void setFeatureId(String featureId) {
+		this.featureId = featureId;
+	}
+
+	public String getRequirementId() {
+		return requirementId;
+	}
+
+	public void setRequirementId(String requirementId) {
+		this.requirementId = requirementId;
+	}
+
+	public String getTestCaseId() {
+		return testCaseId;
+	}
+
+	public void setTestCaseId(String testCaseId) {
+		this.testCaseId = testCaseId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getSteps() {
+		return steps;
+	}
+
+	public void setSteps(String steps) {
+		this.steps = steps;
+	}
+
+	public String getTestCaseype() {
+		return testCaseype;
+	}
+
+	public void setTestCaseype(String testCaseype) {
+		this.testCaseype = testCaseype;
+	}
+
+	public String getTestInput() {
+		return testInput;
+	}
+
+	public void setTestInput(String testInput) {
+		this.testInput = testInput;
+	}
+
+	public String getExpectedResult() {
+		return expectedResult;
+	}
+
+	public void setExpectedResult(String expectedResult) {
+		this.expectedResult = expectedResult;
+	}
+
+	public String getActualResult() {
+		return actualResult;
+	}
+
+	public void setActualResult(String actualResult) {
+		this.actualResult = actualResult;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+
+	public String getSeverity() {
+		return severity;
+	}
+
+	public void setSeverity(String severity) {
+		this.severity = severity;
+	}
+
+	public String getBugComment() {
+		return bugComment;
+	}
+
+	public void setBugComment(String bugComment) {
+		this.bugComment = bugComment;
+	}
+
+}
