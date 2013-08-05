b3b004ceb4b9819722c214dfa95c3b2c3919a0bf
saravanan-na <saravanan.natarajan@photoninfotech.net>
2013-08-05 12:03:06 +0530

    Removed unwanted method

--- src/main/java/com/photon/phresco/impl/EnvironmentsParameterImpl.java
+++ src/main/java/com/photon/phresco/impl/EnvironmentsParameterImpl.java
@@ -77,8 +77,7 @@ public class EnvironmentsParameterImpl implements DynamicParameter, Constants {
     	}
     	
     	String projectDirectory = applicationInfo.getAppDirName();
-    	String eclipsehome = (String) paramsMap.get(ECLIPSE_HOME);
-		String configPath = getConfigurationPath(projectDirectory, eclipsehome).toString();
+    	String configPath = getConfigurationPath(projectDirectory).toString();
     	ConfigManager configManager = new ConfigManagerImpl(new File(configPath)); 
     	List<Environment> environments = configManager.getEnvironments();
     	for (Environment environment : environments) {
@@ -98,21 +97,16 @@ public class EnvironmentsParameterImpl implements DynamicParameter, Constants {
     	return possibleValues;
     }
     
-    private StringBuilder getConfigurationPath(String projectDirectory, String eclipsehome) {
-		StringBuilder builder = new StringBuilder();
-		if (StringUtils.isNotEmpty(eclipsehome)) {
-			builder.append(eclipsehome);
-		} else {
-			builder.append(Utility.getProjectHome());
-			builder.append(projectDirectory);
-		}
-		builder.append(File.separator);
-		builder.append(DOT_PHRESCO_FOLDER);
-		builder.append(File.separator);
-		builder.append(CONFIGURATION_INFO_FILE);
-		
-		return builder;
-	}
+    private StringBuilder getConfigurationPath(String projectDirectory) {
+		 StringBuilder builder = new StringBuilder(Utility.getProjectHome());
+		 builder.append(projectDirectory);
+		 builder.append(File.separator);
+		 builder.append(DOT_PHRESCO_FOLDER);
+		 builder.append(File.separator);
+		 builder.append(CONFIGURATION_INFO_FILE);
+		 
+		 return builder;
+	 }
     
     private String getSettingsPath(String customer) {
     	return Utility.getProjectHome() + customer +"-settings.xml";
