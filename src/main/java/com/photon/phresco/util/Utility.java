ae67c1ca359f291480c1d09dd8679ff4eaac0799
jagadeesh-r <jagadeesh.reddy@photoninfotech.net>
2013-08-27 20:01:14 +0530

    [artf756750]: Framework-3.0->CI configurations are removed automatically..

--- src/main/java/com/photon/phresco/util/Utility.java
+++ src/main/java/com/photon/phresco/util/Utility.java
@@ -662,6 +662,26 @@ public final class Utility implements Constants {
         }
 	}
 
+	public static String getCiJobInfoPath(String appDir) throws PhrescoException {
+		try {
+			StringBuilder builder = new StringBuilder(Utility.getProjectHome());
+			if (!StringUtils.isEmpty(appDir)) {		
+				builder.append(appDir);
+				builder.append(File.separator);
+				builder.append(DOT_PHRESCO_FOLDER);
+				builder.append(File.separator);	
+			}
+			builder.append("ciJob.info");
+			File ciJobInfoFile = new File(builder.toString());
+			if(!ciJobInfoFile.exists()) {
+				ciJobInfoFile.createNewFile();
+			}
+			return ciJobInfoFile.getPath();
+		} catch (IOException e) {
+			throw new PhrescoException(e);
+		}
+	}
+	
 	public static List<ProjectDelivery> getProjectDeliveries(File projectDeliveryFile) throws PhrescoException {
 			FileReader ProjectDeliveryFileReader = null;
 			BufferedReader br = null;
@@ -677,6 +697,7 @@ public final class Utility implements Constants {
 				Gson gson = new Gson();
 				ProjectDelivery = gson.fromJson(br, type);
 			} catch (Exception e) {
+				e.printStackTrace();
 				throw new PhrescoException(e);
 			} finally {
 				Utility.closeStream(br);
