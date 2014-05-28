4984a98a2d0b36961641e0903f3ce5a9fdcafadf
subbareddyvenna <vsreddy2863@gmail.com>
2014-05-28 19:24:40 +0530

    commit

--- src/main/java/com/photon/phresco/impl/CommonApplicationProcessor.java
+++ src/main/java/com/photon/phresco/impl/CommonApplicationProcessor.java
@@ -20,6 +20,7 @@ package com.photon.phresco.impl;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
@@ -28,16 +29,24 @@ import org.codehaus.plexus.util.StringUtils;
 import com.photon.phresco.commons.model.ApplicationInfo;
 import com.photon.phresco.commons.model.ArtifactGroup;
 import com.photon.phresco.commons.model.ProjectInfo;
+import com.photon.phresco.commons.model.ArtifactGroup.Type;
 import com.photon.phresco.exception.PhrescoException;
+import com.photon.phresco.util.ComponentMerge;
+import com.photon.phresco.util.Constants;
 import com.photon.phresco.util.ProjectUtils;
 import com.photon.phresco.util.Utility;
+import com.phresco.pom.exception.PhrescoPomException;
+import com.phresco.pom.util.PomProcessor;
+
 
 public class CommonApplicationProcessor extends AbstractApplicationProcessor {
 
 	@Override
 	public void postUpdate(ApplicationInfo appInfo,
-		List<ArtifactGroup> artifactGroups, List<ArtifactGroup> deletedFeatures) throws PhrescoException {
-		System.out.println("****************** inside the Commmon application processor ******");
+			List<ArtifactGroup> artifactGroups,
+			List<ArtifactGroup> deletedFeatures) throws PhrescoException {
+		System.out.println("****************** inside the Commmon application processor **********");
+
 		String rootModulePath = "";
 		String subModuleName = "";
 		if (StringUtils.isNotEmpty(appInfo.getRootModule())) {
@@ -46,21 +55,38 @@ public class CommonApplicationProcessor extends AbstractApplicationProcessor {
 		} else {
 			rootModulePath = Utility.getProjectHome() + appInfo.getAppDirName();
 		}
-		
-		File phrescoPomFile = Utility.getPomFileLocation(rootModulePath, subModuleName);
+		File phrescoPomFile = Utility.getPomFileLocation(rootModulePath,subModuleName);
+
 		ProjectInfo projectInfo = Utility.getProjectInfo(rootModulePath, "");
-		File sourceFolderLocation = Utility.getSourceFolderLocation(projectInfo, rootModulePath, subModuleName);
+		File sourceFolderLocation = Utility.getSourceFolderLocation( projectInfo, rootModulePath, subModuleName);
 		File pomFile = new File(sourceFolderLocation.getPath() + File.separator + appInfo.getPomFile());
+	    PomProcessor processor;
 		ProjectUtils projectUtils = new ProjectUtils();
-		
+        
 		projectUtils.deletePluginExecutionFromPom(phrescoPomFile);
-		if(CollectionUtils.isNotEmpty(artifactGroups)) {
-			projectUtils.updatePOMWithPluginArtifact(pomFile, phrescoPomFile, artifactGroups);
-		}
 		
-		if(CollectionUtils.isNotEmpty(deletedFeatures)) {
+		if (CollectionUtils.isNotEmpty(artifactGroups)) {
+			projectUtils.updatePOMWithPluginArtifact(pomFile, phrescoPomFile,artifactGroups);
+		}
+
+		try {
+			processor = new PomProcessor(pomFile);
+			String isMerge = processor.getPropertyValue(Constants.POM_PROP_KEY_COMPONENT_MERGE).trim();
+			if (CollectionUtils.isNotEmpty(artifactGroups) && isMerge.equalsIgnoreCase("true")) {
+				projectUtils.deleteResourcePluginExecutionFromPom(phrescoPomFile);
+				projectUtils.deleteCleanPluginExecutionFromPom(phrescoPomFile);
+				projectUtils.updatePOMWithMergePlugins(pomFile, phrescoPomFile, artifactGroups);
+			}
+		
+
+		if (CollectionUtils.isNotEmpty(artifactGroups)) {
+			projectUtils.updatePOMWithPluginDependencies(pomFile,artifactGroups);
+		}
+
+		if (CollectionUtils.isNotEmpty(deletedFeatures)) {
 			projectUtils.deleteFeatureDependencies(pomFile, deletedFeatures);
 		}
+
 		BufferedReader breader = projectUtils.ExtractFeature(phrescoPomFile);
 		try {
 			String line = "";
@@ -72,5 +98,47 @@ public class CommonApplicationProcessor extends AbstractApplicationProcessor {
 		} catch (IOException e) {
 			throw new PhrescoException(e);
 		}
+		
+		if(isMerge.equalsIgnoreCase("true")){
+			ComponentMerge.mergeAndUnMerge(pomFile,sourceFolderLocation,artifactGroups,deletedFeatures);
+			
+	    	List<ArtifactGroup> artifacts = new ArrayList<ArtifactGroup>();	
+		    for (ArtifactGroup artifactGroup : artifactGroups) {			
+			
+			if(artifactGroup.getType().name().equals(Type.COMPONENT.name())) {
+		
+			  artifacts.add(artifactGroup);
+			  }
+		  }
+		
+		 if(CollectionUtils.isNotEmpty(artifacts) && phrescoPomFile.exists()) {
+		    projectUtils.updateToCleanPlugin(phrescoPomFile, artifacts);
+
+		  } else {
+         
+		    projectUtils.updateToCleanPlugin(pomFile, artifacts);
+
+		}
+		 
+	   BufferedReader bufferreader = ComponentMerge.cleanComponents(phrescoPomFile);
+		 
+		 try {
+				String line = "";
+				while ((line = bufferreader.readLine()) != null) {
+					if (line.startsWith("[ERROR]")) {
+						System.err.println(line);
+					}
+				}
+			} catch (IOException e) {
+				throw new PhrescoException(e);
+			}
+		} 
+		 
+		} catch (PhrescoPomException e1) {
+			throw new PhrescoException(e1);
+		}
+		
 	}
+	
+   
 }
